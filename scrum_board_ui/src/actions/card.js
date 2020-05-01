85fbac78b9b75214e30847154b2e5c59713047c6
kirandash <thekirankumardash@gmail.com>
2020-05-01 20:32:41 +0800

    Scrum Board - 7.4 Creating Redux Actions for Create Card and Remove Card

--- scrum_board_ui/src/actions/card.js
+++ scrum_board_ui/src/actions/card.js
@@ -8,13 +8,13 @@ export const actionTypes = {
 }; // Export Action Types so that it can be imported in other files
 
 // CREATE_CARD Action Creator
-export const createCard = title => ({
+export const createCard = (title, listId) => ({
     type: CREATE_CARD, // Type here can be the string 'CREATE_CARD' but it will increase chances of typo. Thus, we are creating a constant first. So if constant is spelled wrong, our editor will throw warning
-    payload: { title }
+    payload: { title, listId }
 }); // Action Creator: A fn that returns 2 params: type of action and payload
 
 // REMOVE_CARD Action Creator
-export const removeCard = title => ({
+export const removeCard = (title, listId) => ({
     type: REMOVE_CARD,
-    payload: { title }
+    payload: { title, listId }
 }); // Action Creator: A fn that returns 2 params: type of action and payload
